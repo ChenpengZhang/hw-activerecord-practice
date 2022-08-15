87a90afc1e168f7e97a2e3cd90175934eb2e0743
ZCP <zhangchenpeng2003@163.com>
2022-08-15 18:15:34 +0800

    Pass all tests

--- lib/activerecord_practice.rb
+++ lib/activerecord_practice.rb
@@ -18,10 +18,56 @@ class Customer < ActiveRecord::Base
 
   def self.any_candice
     # YOUR CODE HERE to return all customer(s) whose first name is Candice
+    Customer.where(:first => 'Candice')
     # probably something like:  Customer.where(....)
   end
   def self.with_valid_email
     # YOUR CODE HERE to return only customers with valid email addresses (containing '@')
+    Customer.where('email LIKE ?', "%@%")
   end
   # etc. - see README.md for more details
+  
+  def self.with_dot_org_email
+    Customer.where('email LIKE ?', "%.org%")
+  end
+
+  def self.with_invalid_email
+    Customer.where('email NOT LIKE ?', "%@%")
+  end
+
+  def self.with_blank_email
+    Customer.where('email IS NULL')
+  end
+
+  def self.born_before_1980
+    Customer.where("cast(strftime('%Y', birthdate) as int) < ?", 1980)
+  end
+
+  def self.with_valid_email_and_born_before_1980
+    Customer.with_valid_email.born_before_1980
+  end
+
+  def self.last_names_starting_with_b
+    Customer.where("last LIKE ?", "b%").order(:birthdate)
+  end
+
+  def self.twenty_youngest
+    Customer.order(birthdate: :desc).limit(20)
+  end
+
+  def self.update_gussie_murray_birthdate
+    Customer.where("first = 'Gussie' AND last = 'Murray'").first.update(birthdate: '2004-02-08')
+  end
+
+  def self.change_all_invalid_emails_to_blank
+    Customer.with_invalid_email.update(email: '')
+  end
+
+  def self.delete_meggie_herman
+    Customer.where("first = 'Meggie' AND last = 'Herman'").destroy_all
+  end
+
+  def self.delete_everyone_born_before_1978
+    Customer.where('birthdate < ?', Time.parse("1 January 1978")).destroy_all
+  end
 end
